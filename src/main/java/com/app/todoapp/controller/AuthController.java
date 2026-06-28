fde88394eadbac3d8eecbfe7e530baa5756e0fb8
jojangandha-saha <sj6007700@gmail.com>
2026-06-28 13:20:31 +0530

    minor chanes in authcontroller

--- src/main/java/com/app/todoapp/controller/AuthController.java
+++ src/main/java/com/app/todoapp/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.app.todoapp.dto.RegisterRequest;
 import com.app.todoapp.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-
-    public AuthController(UserService userService) {
+    private final AuthenticationManager authenticationManager;
+    public AuthController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/register")
@@ -24,6 +27,19 @@ public class AuthController {
         userService.register(request);
 
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @Valid @RequestBody LoginRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                ));
+
+        return ResponseEntity.ok("Login successfully");
     }
 
 }
